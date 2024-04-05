669bce960ad5932397903dc77ae434ea0388e5c0
chinhle23 <112718475+chinhle23@users.noreply.github.com>
2024-04-05 05:03:44 -0400

    Define make_guess method to build @guess array

--- mastermind.rb
+++ mastermind.rb
@@ -1,22 +1,39 @@
 module Mastermind
   COLORS = ["red", "orange", "yellow", "green", "blue", "violet"]
   class Game
-    attr_accessor :guess
     def initialize
       @code = []
       @guess = []
     end
     def play
       randomize_code
-      p @code
+      make_guess
+      p @guess
     end
     private
+    attr_accessor :guess
     attr_reader :code
     def randomize_code
       4.times do
         @code.push(COLORS[rand(0..5)])
       end
     end
+    def make_guess
+      i = 1
+      loop do
+        puts "Make guess #{COLORS} for position #{i}"
+        begin
+          guess = gets.chomp
+          raise unless COLORS.include?(guess)
+        rescue StandardError
+          puts 'Invalid input! Try again...'
+        else 
+          @guess.push(guess)
+          i += 1
+          break if i == 5
+        end
+      end
+    end  
   end
 end
 

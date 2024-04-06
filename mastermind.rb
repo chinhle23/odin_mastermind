232a77b9148269337e9d863ae20567d781adeb28
chinhle23 <112718475+chinhle23@users.noreply.github.com>
2024-04-06 15:31:41 -0400

    Refactor current methods into appropriate HumanPlayer & ComputerPlayer classes

--- mastermind.rb
+++ mastermind.rb
@@ -3,31 +3,34 @@
 # This houses the components of the Mastermind game
 module Mastermind
   COLORS = ["red", "orange", "yellow", "green", "blue", "violet"]
+  ROLES = ["creator", "guesser"]
   class Game
-    @@guesses_remaining = 12
-    @@tries = 0
-    def initialize
-      @code = []
-      @guess = []
-      @clue = []
+    attr_accessor :guesses_remaining, :tries
+    attr_reader :players 
+
+    def initialize(player_1, player_2)
+      @guesses_remaining = 12
+      @tries = 0
+      @players = [player_1.new(self), player_2.new(self)]
     end
 
     def play
-      randomize_code
+      @players[0].choose_role
+      @players[1].randomize_secret_code if @players[0].role == ROLES[1]
       loop do
-        make_guess
+        @players[0].guess_colors
         if correct_guess?
-          puts "You guessed correctly (#{@code}) in #{@@tries} #{@@tries > 1 ? 'tries' : 'try'}"
+          puts "You guessed correctly (#{@players[1].secret_code}) in #{@tries} #{@tries > 1 ? 'tries' : 'try'}"
           return
-        elsif @@guesses_remaining == 0
-          puts "Game over. You ran out of guesses(#{@@tries})."
-          puts "The code was #{@code}"
+        elsif @guesses_remaining == 0
+          puts "Game over. You ran out of guesses(#{@tries})."
+          puts "The code was #{@players[1].secret_code}"
           return
         else
           give_guess_result
-          give_clue(@code, @guess)
-          @guess = []
-          @clue = []
+          @players[1].give_clue(@players[1].secret_code, @players[0].guess)
+          @players[0].guess = []
+          @players[1].clue = []
         end
       end
     end
@@ -35,18 +38,45 @@ module Mastermind
     private
 
     attr_accessor :guess, :clue
-    attr_reader :code
 
-    def randomize_code
-      4.times do
-        @code.push(COLORS[rand(0..5)])
+    def correct_guess?
+      @players[0].guess == @players[1].secret_code
+    end
+
+    def give_guess_result
+      puts "Your guess: #{@players[0].guess} is incorrect. (#{@guesses_remaining} #{@guesses_remaining > 1 ? 'guesses' : 'guess'} remaining)"
+    end
+  end
+
+  class Player
+    attr_accessor :role, :secret_code, :guess
+
+    def initialize(game)
+      @game = game
+      @role = ''
+      @secret_code = []
+      @guess = []
+    end
+  end
+
+  class HumanPlayer < Player
+    def choose_role
+      puts "Choose a role: #{ROLES}"
+      begin
+        role = gets.chomp
+        raise unless ROLES.include?(role)
+      rescue StandardError
+        puts 'Invalid input! Try again...'
+      else 
+        @game.players[0].role = role
+        @game.players[1].role = role == ROLES[0] ? ROLES[1] : ROLES[0]
       end
     end
 
-    def make_guess
+    def guess_colors
       i = 1
       loop do
-        puts "Guess one of #{COLORS} for position #{i}"
+        puts "Possible colors: #{COLORS}\nGuess a color for position #{i}"
         begin
           guess = gets.chomp
           raise unless COLORS.include?(guess)
@@ -58,35 +88,42 @@ module Mastermind
           break if i == 5
         end
       end
-      @@tries += 1
-      @@guesses_remaining -= 1
+      @game.tries += 1
+      @game.guesses_remaining -= 1
     end
-    
-    def correct_guess?
-      @code == @guess
+  end
+
+  class ComputerPlayer < Player
+    attr_accessor :clue
+
+    def initialize(game)
+      super
+      @clue = []
     end
 
-    def give_guess_result
-      puts "Your guess: #{@guess} is incorrect. (#{@@guesses_remaining} #{@@guesses_remaining > 1 ? 'guesses' : 'guess'} remaining)"
+    def randomize_secret_code
+      4.times do
+        @secret_code.push(COLORS[rand(0..5)])
+      end
     end
 
-    def give_clue(code, guess)
-      incorrect = {guess: [], code: []}
+    def give_clue(secret_code, guess)
+      incorrect = {guess: [], secret_code: []}
 
       # looks for matches first then include incorrect guesses in the 'incorrect' hash
       guess.each_index do |i| 
-        if guess[i] == code[i]
+        if guess[i] == secret_code[i]
           @clue.push("correct")
         else
           @clue.push("incorrect")
           incorrect[:guess][i] = guess[i]
-          incorrect[:code][i] = code[i]
+          incorrect[:secret_code][i] = secret_code[i]
         end
       end
 
       # looks for colors in the wrong positions
       incorrect[:guess].each_index do |i|
-        if incorrect[:code].include?(incorrect[:guess][i]) && incorrect[:guess][i] != nil
+        if incorrect[:secret_code].include?(incorrect[:guess][i]) && incorrect[:guess][i] != nil
           @clue[i] = "wrong spot"
         end
       end
@@ -98,4 +135,4 @@ end
 
 include Mastermind
 
-Game.new.play
+Game.new(HumanPlayer, ComputerPlayer).play

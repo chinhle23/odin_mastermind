28331eeb04074fa194c540511f32b69a4136b69b
chinhle23 <112718475+chinhle23@users.noreply.github.com>
2024-04-09 07:50:16 -0400

    Refactor guess_colors method to allow ComputerPlayer to guess HumanPlayer's secret code

--- mastermind.rb
+++ mastermind.rb
@@ -1,9 +1,11 @@
 # frozen_string_literal: true
+require 'pry-byebug'
 
 # This houses the components of the Mastermind game
 module Mastermind
   COLORS = ["red", "orange", "yellow", "green", "blue", "violet"]
   ROLES = ["creator", "guesser"]
+  CLUES = ["O", "X", "C"]
   class Game
     attr_accessor :guesses_remaining, :tries
     attr_reader :players 
@@ -20,7 +22,7 @@ module Mastermind
         @players[1].randomize_secret_code 
         loop do
           @players[0].guess_colors(@players[0].guess)
-          if correct_guess?
+          if human_correct_guess?
             puts "You guessed correctly (#{@players[1].secret_code}) in #{@tries} #{@tries > 1 ? 'tries' : 'try'}"
             return
           elsif @guesses_remaining == 0
@@ -36,7 +38,22 @@ module Mastermind
         end
       else
         @players[0].build_secrect_code(@players[0].secret_code)
-        p @players[0].secret_code 
+        COLORS.each_index do |i|
+          @players[1].guess_colors(@players[0].secret_code, @players[1].guess, @players[0].previous_clue, COLORS[i])
+          if computer_correct_guess?
+            puts "Computer guessed correctly (#{@players[0].secret_code}) in #{@tries} #{@tries > 1 ? 'tries' : 'try'}"
+            return
+          elsif @guesses_remaining == 0
+            puts "Game over. You ran out of guesses(#{@tries})."
+            puts "The code was #{@players[0].secret_code}"
+            return
+          else
+            give_guess_result
+            @players[0].give_clue(@players[0].secret_code, @players[1].guess)
+            @players[0].previous_clue = @players[0].clue
+            @players[0].clue = []
+          end
+        end
       end
     end
 
@@ -44,27 +61,68 @@ module Mastermind
 
     attr_accessor :guess, :clue
 
-    def correct_guess?
+    def human_correct_guess?
       @players[0].guess == @players[1].secret_code
     end
 
+    def computer_correct_guess?
+      @players[1].guess == @players[0].secret_code
+    end
+
     def give_guess_result
-      puts "Your guess: #{@players[0].guess} is incorrect. (#{@guesses_remaining} #{@guesses_remaining > 1 ? 'guesses' : 'guess'} remaining)"
+      puts "#{@players[0].role == ROLES[1] ? "Your guess: #{@players[0].guess}" : "Computer's guess: #{@players[1].guess}"} is incorrect. (#{@guesses_remaining} #{@guesses_remaining > 1 ? 'guesses' : 'guess'} remaining)"
     end
   end
 
   class Player
-    attr_accessor :role, :secret_code, :guess
+    attr_accessor :role, :secret_code, :guess, :clue
 
     def initialize(game)
       @game = game
       @role = ''
       @secret_code = []
       @guess = []
+      @clue = []
+    end
+
+    def give_clue(secret_code, guess)
+      incorrect = {guess: [], secret_code: []}
+
+      # looks for matches first then include incorrect guesses in the 'incorrect' hash
+      guess.each_index do |i| 
+        if guess[i] == secret_code[i]
+          @clue.push(CLUES[0])
+        else
+          @clue.push(CLUES[1])
+          incorrect[:guess][i] = guess[i]
+          incorrect[:secret_code][i] = secret_code[i]
+        end
+      end
+
+      # looks for colors in the wrong positions
+      incorrect[:guess].each_index do |i|
+        if incorrect[:secret_code].include?(incorrect[:guess][i]) && incorrect[:guess][i] != nil
+          @clue[i] = CLUES[2]
+        end
+      end
+
+      puts "---------------------------------------"
+      puts "Here's a clue: #{@clue}"
+      puts "O - exact match"
+      puts "X - wrong color"
+      puts "C - correct color, wrong position"
+      puts "---------------------------------------"
     end
   end
 
   class HumanPlayer < Player
+    attr_accessor :previous_clue
+
+    def initialize(game)
+      super
+      @previous_clue = []
+    end
+
     def choose_role
       loop do
         puts "Choose a role: #{ROLES}"
@@ -112,42 +170,36 @@ module Mastermind
   end
 
   class ComputerPlayer < Player
-    attr_accessor :clue
-
-    def initialize(game)
-      super
-      @clue = []
-    end
-
     def randomize_secret_code
       4.times do
         @secret_code.push(COLORS[rand(0..5)])
       end
     end
 
-    def give_clue(secret_code, guess)
-      incorrect = {guess: [], secret_code: []}
-
-      # looks for matches first then include incorrect guesses in the 'incorrect' hash
-      guess.each_index do |i| 
-        if guess[i] == secret_code[i]
-          @clue.push("correct")
-        else
-          @clue.push("incorrect")
-          incorrect[:guess][i] = guess[i]
-          incorrect[:secret_code][i] = secret_code[i]
-        end
-      end
-
-      # looks for colors in the wrong positions
-      incorrect[:guess].each_index do |i|
-        if incorrect[:secret_code].include?(incorrect[:guess][i]) && incorrect[:guess][i] != nil
-          @clue[i] = "wrong spot"
-        end
-      end
-
-      puts "Here's your clue: #{@clue}"
+    def guess_colors(secrect_code, guess, clue, color)
+      build_color_combo(secrect_code, guess, clue, color)
+      @game.tries += 1
+      @game.guesses_remaining -= 1
     end
+
+    private
+
+    def build_color_combo(secrect_code, guess, clue, color)
+      if clue == []
+        4.times do 
+          guess.push(COLORS[0])
+        end
+      else  
+        clue.each_index do |i|
+          if clue[i] == "O"
+            guess[i] = secrect_code[i]
+          else
+            guess[i] = color
+          end
+        end
+      end 
+    end
+
   end
 end
 
